32613fb45b2ca0c403a9aee2efe2dc6768056db0
marcos vinicio vailant oliveira <marcos.oliveira157@senaisp.edu.br>
2026-09-16 07:43:36 -0300

    09.09.26

--- 2°Termo/BACKEND/avaliação_somativa/ex01_producao.js
+++
@@ -1,7 +0,0 @@
-const pecasPorHora = 120;
-const horasDoTurno = 8;
-const producaoTotal = pecasPorHora * horasDoTurno;
-
-console.log(
-	`A produção é de ${pecasPorHora} peças por hora durante ${horasDoTurno} horas, totalizando ${producaoTotal} peças no turno.`
-);
--- 2°Termo/BACKEND/avaliação_somativa/ex02_compra.js
+++
@@ -1,8 +0,0 @@
-const entrada = require('readline-sync');
-
-const nomematerial= entrada.question("QUAL O NOME DO MATERIAL?:")
-const quantidadecomprada= entrada.questionInt("QUAL A QUANTIDADE COMPRADA")
-const precounitario= entrada.questionFloat("QUAL O VALOR UNITARIO")
-const valortotal= (quantidadecomprada+precounitario)
-
- console.log(`o seu ${nomematerial} deu o total de ${valortotal}!! `)
